c9fe5186e3e0d80e1f2b530a6f616671b89a76e0
Kendra Noneman <knoneman@br011.ib.bridges2.psc.edu>
2023-04-11 19:23:29 -0400

    bridges updates

--- bridges/bridges_submit.sh
+++ bridges/bridges_submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -p batch
-#SBATCH -J C60
-#SBATCH -o outfiles/C60.o%j
+#SBATCH -J neuraldecoding
+#SBATCH -o runs/outfiles/out.o%j
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --mail-type=all
@@ -9,8 +9,10 @@
 #SBATCH -t 24:00:00
 #SBATCH --gres=gpu:1
 
-source activate py3.9.5
+module purge
+source activate py3.10.8
+module load numpy
 
-python decoding-1.py $1
+python ../decoding-1.py $1
 #cp files you'd like to move off of scratch
 #mv files that you'd like moved off of scratch

96a3ee50976efaad4b7db852097b70c9dccdbc25
Kendra Noneman <knoneman@br013.ib.bridges2.psc.edu>
2023-04-12 13:10:20 -0400

    first run on cluster

--- bridges_submit.sh
+++ bridges_submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -p RM-shared
 #SBATCH -J neuraldecoding
-#SBATCH -o outfiles/out.o%j
+#SBATCH -o runs/outfiles/out.o%j
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --mail-type=all
@@ -13,8 +13,6 @@ module purge
 module load anaconda3/2022.10
 conda activate neuraldecoding
 
-module load /jet/home/knoneman/NeuralDecoding/handy_functions
-
-python ../decoding-1.py $1
+python decoding-1.py $1
 #cp files you'd like to move off of scratch
 #mv files that you'd like moved off of scratch

8a7bc5414a4b2b1bdbcf83043c49136bbaeb680c
Kendra Noneman <knoneman@br011.ib.bridges2.psc.edu>
2023-04-17 16:09:00 -0400

    changing submit

--- bridges_submit.sh
+++ bridges_submit.sh
@@ -3,10 +3,10 @@
 #SBATCH -J neuraldecoding
 #SBATCH -o runs/outfiles/out.o%j
 #SBATCH -N 1
-#SBATCH -n 5
+#SBATCH -n 1
 #SBATCH --mail-type=all
 #SBATCH --mail-user=knoneman@andrew.cmu.edu
-#SBATCH -t 00:01:00
+#SBATCH -t 05:00:00
 #SBATCH --ntasks-per-node=8
 
 module purge
@@ -14,10 +14,6 @@ module load anaconda3/2022.10
 module load cuda/11.4
 conda activate neuraldecoding
 
-python decoding-2.py $1 0
-python decoding-2.py $1 1
-python decoding-2.py $1 2
-python decoding-2.py $1 3
-python decoding-2.py $1 4
+python decoding-2.py $1 $2
 #cp files you'd like to move off of scratch
 #mv files that you'd like moved off of scratch

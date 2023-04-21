c24b414cdf76f47bb1183d6896ded7e5c246d075
kendranoneman <knoneman@andrew.cmu.edu>
2023-04-21 16:56:27 -0500

    reverting back farther

--- decodingSetup.py
+++ decodingSetup.py
@@ -7,7 +7,6 @@ import time
 import pandas as pd
 import os.path
 import os
-import random
 
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
@@ -40,55 +39,51 @@ def get_dataParams(linenum):
 
     with open(cwd+'/datasets/vars-'+sess+'.pickle','rb') as f:
         neural_data,out_binned=pickle.load(f,encoding='latin1')
-
+      
     units = pd.read_csv(cwd+'/datasets/units-'+sess_nodt+'.csv')
 
     [bins_before,bins_current,bins_after] = helpers.get_bins(bn)
 
     ############ getting all possible combinations of neurons #############
 
-    mt = sorted(units.loc[units['BrainArea'] == 'MT'].index)
-    fef = sorted(units.loc[units['BrainArea'] == 'FEF'].index)
-
     mt_inds = []
     fef_inds = []
 
-    for row in range(10):
-        mt_inds.append(sorted(np.random.choice(mt, nm, replace=False)))
-        fef_inds.append(sorted(np.random.choice(fef, nf, replace=False)))
+    mt = sorted(units.loc[units['BrainArea'] == 'MT'].index)
+    fef = sorted(units.loc[units['BrainArea'] == 'FEF'].index)
 
+    mt_set = itertools.combinations(mt, len(mt))
+    fef_set = itertools.combinations(fef, len(fef))
 
-    #mt_set = itertools.combinations(mt, nm)
-    #fef_set = itertools.combinations(fef, nf)
+    for i in mt_set:
+        mt_inds.append(i)
+    for i in fef_set:
+        fef_inds.append(i)
 
-    #for i in mt_set:
-    #    mt_inds.append(i)
-    #for i in fef_set:
-    #    fef_inds.append(i)
-
+    neural_data = neural_data[:,sorted(np.concatenate((np.array((mt_inds[0])),np.array((fef_inds[0])))))];
     out_binned = out_binned[:,helpers.define_outputs(o)]
+
+    X = get_spikes_with_history(neural_data,bins_before,bins_after,bins_current)
+    num_examples=X.shape[0]
+    X_flat=X.reshape(X.shape[0],(X.shape[1]*X.shape[2]))
+    y=out_binned
+
+    ########### cross-validation ###############3
     outer_cv = KFold(n_splits=fo, random_state=None, shuffle=False)
 
-    X_train0 = [[] for i in range(fo)]
-    X_flat_train0 = [[] for i in range(fo)]
-    y_train0 = [[] for i in range(fo)]
-    X_test = [[] for i in range(fo)]
-    X_flat_test = [[] for i in range(fo)]
-    y_test = [[] for i in range(fo)]
-    for r in range(len(fef_inds)):
-        neural_data2 = neural_data[:,sorted(np.concatenate((np.array((mt_inds[r])),np.array((fef_inds[r])))))];
-        X = get_spikes_with_history(neural_data2,bins_before,bins_after,bins_current)
-        num_examples=X.shape[0]
-        X_flat=X.reshape(X.shape[0],(X.shape[1]*X.shape[2]))
-        y=out_binned
+    X_train0 = [] 
+    X_flat_train0 = [] 
+    y_train0 = []
+    X_test = []
+    X_flat_test = []
+    y_test = []
+    for i, (train0_index, test_index) in enumerate(outer_cv.split(X)):
+        X_train0.append(X[train0_index,:,:])
+        X_flat_train0.append(X_flat[train0_index,:])
+        y_train0.append(y[train0_index,:])
 
-        for i, (train0_index, test_index) in enumerate(outer_cv.split(X)):
-            X_train0[i].append(X[train0_index,:,:])
-            X_flat_train0[i].append(X_flat[train0_index,:])
-            y_train0[i].append(y[train0_index,:])
-            X_test[i].append(X[test_index,:,:])
-            X_flat_test[i].append(X_flat[test_index,:])
-            y_test[i].append(y[test_index,:])
+        X_test.append(X[test_index,:,:])
+        X_flat_test.append(X_flat[test_index,:])
+        y_test.append(y[test_index,:])
 
-
-    return X_train0, X_flat_train0, y_train0, X_test, X_flat_test, y_test
+    return X_train0, X_flat_train0, y_train0, X_test, X_flat_test, y_test 

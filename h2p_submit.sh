c4b35f448202fe748ca5f6feada8e3c605d55bd7
knoneman <knoneman@login0.crc.pitt.edu>
2023-04-28 19:41:07 -0400

    adding parallelization

--- h2p_submit.sh
+++ h2p_submit.sh
@@ -2,18 +2,21 @@
 #SBATCH --cluster=smp
 #SBATCH --partition=smp
 #SBATCH --job-name=neuraldecoding
-#SBATCH -o runs/outfiles/out.o%j
+#SBATCH --o runs/outfiles/out.o%j
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --mail-type=all
 #SBATCH --mail-user=knoneman@pitt.edu
 #SBATCH --time=0-00:20:00
-#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
 
 module purge
 module load gcc/8.2.0
 module load python/anaconda3.9-2021.11
 conda activate neuraldecoding
 
-python decoding-2.py $1 $2
+export p=$SLURM_CPUS_PER_TASK # Size of multiprocessing pool
+
+python decoding-2.py $1 $2 $p
 #cp files you'd like to move off of scratch
 #mv files that you'd like moved off of scratch

e46bf9fb51451df12dec752c75f85a2d25632a35
Kendra Noneman <knoneman@br013.ib.bridges2.psc.edu>
2023-04-30 17:07:02 -0400

    works on bridges now

--- bridges_submit.sh
+++ bridges_submit.sh
@@ -6,12 +6,20 @@
 #SBATCH --mail-type=all
 #SBATCH --mail-user=knoneman@andrew.cmu.edu
 #SBATCH -t 00:30:00
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --array=0-10
+
+echo "My SLURM_ARRAY_JOB_ID is $SLURM_ARRAY_JOB_ID."
+echo "My SLURM_ARRAY_TASK_ID is $SLURM_ARRAY_TASK_ID"
+echo "Executing on the machine:" $(hostname)
 
 module purge
 module load anaconda3/2022.10
 conda activate neuraldecoding
 
-python decoding-2.py $1 $2
+echo "num cpus is $SLURM_CPUS_PER_TASK" # Size of multiprocessing pool
+
+python decoding-2.py $1
+
 #cp files you'd like to move off of scratch
 #mv files that you'd like moved off of scratch

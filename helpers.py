3970bc103b467561e00121efdbd0de0b78a3939e
kendranoneman <knoneman@andrew.cmu.edu>
2023-05-14 14:47:50 -0400

    little changes to helpers

--- helpers.py
+++ helpers.py
@@ -113,3 +113,63 @@ def normalize_trainTest(X_train,X_flat_train,X_test,X_flat_test,y_train,y_test):
     y_zscore_train=y_train/(np.nanstd(y_train,axis=0))
 
     return X_train,X_flat_train,X_test,X_flat_test,y_train,y_test,y_zscore_train,y_zscore_test
+
+def get_dataX(i,repeat,outer_fold):
+    mt_perRepeat, fef_perRepeat = neuronsSample.get_neuronRepeatsX(i)
+    s,t,d,m,o,nm,nf,bn,fo,fi,r = get_params(i)
+    sess,sess_nodt = get_session(s,t,d)
+    [bins_before,bins_current,bins_after] = get_bins(bn)
+
+    with open(cwd+'/datasets/vars-'+sess+'.pickle','rb') as f:
+        neural_data,pos_binned,vel_binned,acc_binned=pickle.load(f,encoding='latin1')
+
+    neural_data2= neural_data[:,neurons_perRepeat[repeat]]
+
+    X = get_spikes_with_history(neural_data2,bins_before,bins_after,bins_current)
+    X = X[range(bins_before,X.shape[0]-bins_after),:,:]
+    num_examples=X.shape[0]
+    X_flat=X.reshape(X.shape[0],(X.shape[1]*X.shape[2]))
+
+    if o==0:
+        y=pos_binned
+    elif o==1:
+        y=vel_binned
+    elif o==2:
+        y=acc_binned
+
+    y = y[range(bins_before,y.shape[0]-bins_after),:]
+
+    outer_cv = KFold(n_splits=fo, random_state=None, shuffle=False)
+    outer_folds = outer_cv.split(X)
+
+    trainTest_index = next(itertools.islice(outer_cv.split(X), outer_fold, None))
+
+    X_train0 = X[trainTest_index[0],:,:]
+    X_flat_train0 = X_flat[trainTest_index[0],:]
+    y_train0 = y[trainTest_index[0],:]
+    X_test = X[trainTest_index[1],:,:]
+    X_flat_test = X_flat[trainTest_index[1],:]
+    y_test = y[trainTest_index[1],:]
+
+    return X_train0,X_flat_train0,y_train0,X_test,X_flat_test,y_test,neurons_perRepeat[repeat]
+
+def get_foldneuronPairs(i):
+    # initialize lists
+    s,t,d,m,o,nm,nf,bn,fo,fi,r = get_params(i)
+    unique_combinations = []
+
+    pairs = list(product(range(fo), range(r)))
+    return pairs
+
+def normalize_trainTest(X_train,X_flat_train,X_test,X_flat_test,y_train,y_test):
+    X_test=(X_test-np.nanmean(X_train,axis=0))/(np.nanstd(X_train,axis=0))
+    X_train=(X_train-np.nanmean(X_train,axis=0))/(np.nanstd(X_train,axis=0))
+    X_flat_test=(X_flat_test-np.nanmean(X_flat_train,axis=0))/(np.nanstd(X_flat_train,axis=0))
+    X_flat_train=(X_flat_train-np.nanmean(X_flat_train,axis=0))/(np.nanstd(X_flat_train,axis=0))
+    y_test=y_test-np.mean(y_train,axis=0)
+    y_train=y_train-np.mean(y_train,axis=0)
+    y_zscore_test=y_test/(np.nanstd(y_train,axis=0))
+    y_zscore_train=y_train/(np.nanstd(y_train,axis=0))
+
+    return X_train,X_flat_train,X_test,X_flat_test,y_train,y_test,y_zscore_train,y_zscore_test
+

8946ce33f322a80d8610e674c16fc90da5b0024a
knoneman <knoneman@andrew.cmu.edu>
2023-06-21 16:03:42 -0400

    some models too big

--- h2p_submit.sh
+++ h2p_submit.sh
@@ -6,10 +6,10 @@
 #SBATCH --error=runs/outfiles/error_%A_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=knoneman@pitt.edu
-#SBATCH --time=0-03:59:59
+#SBATCH --time=0-23:59:59
 #SBATCH --array=0-9
 
 echo "My SLURM_ARRAY_JOB_ID is $SLURM_ARRAY_JOB_ID."

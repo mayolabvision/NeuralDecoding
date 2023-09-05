12490076bad33b3d0f78fe96c0e3c4cbf8418ad9
kendranoneman <knoneman@andrew.cmu.edu>
2023-09-04 21:08:25 -0400

    adding process code

---
+++ process_data.m
@@ -0,0 +1,34 @@
+clear
+clc
+
+tic
+%% Setup folders and parameters
+
+folder = '/Users/kendranoneman/Projects/mayo/NeuralDecoding/datasets';
+dataFolder = '/Users/kendranoneman/Data/neural-decoding/raw';
+
+addpath(genpath('/Users/kendranoneman/Projects/mayo/helperfunctions'))
+
+filelist   =  dir(dataFolder); % files in raw data folder
+name       =  {filelist.name}; 
+sessions   =  name(~strncmp(name, '.', 1))';
+sessions = sessions(16);
+
+%%
+%parpool('local',8);
+warning('off')
+for s=1:length(sessions) % loop through each session
+    sess_name  =  string(sessions{s}); sess_name = char(extractBetween(sess_name,".","."));
+    fprintf(sprintf('%d / %d \n\n',s, length(sessions)));
+
+    %%%%%%%%%%%%%%% Save to a file %%%%%%%%%%%%%%%%%%
+    if not(isfolder(sprintf('%s/%s',folder,sess_name)))
+        mkdir(sprintf('%s/%s',folder,sess_name))
+    end
+
+    [spike_times,outputs,contConditions,unitTbl] = data_formatting(sess_name,sprintf('%s/%s',folder,sess_name));
+end
+
+toc
+load gong.mat
+sound(y)

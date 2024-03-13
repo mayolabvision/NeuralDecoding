ac6ab2d456d4f5e01d5ce0c38f1138f7f89b3af0
knoneman <knoneman@andrew.cmu.edu>
2024-03-13 10:49:40 -0400

    fixing 25ms bin files

--- h2p_submit.sh
+++ h2p_submit.sh
@@ -9,8 +9,8 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=knoneman@pitt.edu
-#SBATCH --time=0-09:29:59
-#SBATCH --array=0-999
+#SBATCH --time=0-19:29:59
+#SBATCH --array=0
 
 echo "My SLURM_ARRAY_JOB_ID is $SLURM_ARRAY_JOB_ID."
 echo "My SLURM_ARRAY_TASK_ID is $SLURM_ARRAY_TASK_ID"
